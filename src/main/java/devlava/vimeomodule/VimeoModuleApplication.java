24fe27e41c0d60d38bfc5faffbcffec3d84ab4ef
lavaspoon <lavaspoon@gmail.com>
2025-04-21 23:36:09 +0900

    init

---
+++ src/main/java/devlava/vimeomodule/VimeoModuleApplication.java
@@ -0,0 +1,13 @@
+package devlava.vimeomodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VimeoModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VimeoModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/devlava/vimeomodule/VimeoModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package devlava.vimeomodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VimeoModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
